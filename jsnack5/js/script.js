17841f0768b86eba98b7c432c27afdb479eaadd5
Federico <federico.caiello@gmail.com>
2020-01-10 18:19:56 +0100

    jsnack5 con while

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,13 +1,26 @@
 // Crea un array vuoto.
-var listaNumeri = [];
+// var listaNumeri = [];
+//
+// // Chiedi per 6 volte all’utente di inserire un numero
+// // se è dispari inseriscilo nell’array. CON FOR
+// for (var i=0; i<6; i++) {
+//   var numeroUtente = parseInt(prompt('inserisci un numero'));
+//   console.log(numeroUtente);
+//   if (numeroUtente%2 != 0){
+//     listaNumeri.push(numeroUtente);
+//     console.log(listaNumeri);
+//   }
+// }
 
-// Chiedi per 6 volte all’utente di inserire un numero
-// se è dispari inseriscilo nell’array. CON FOR
-for (var i=0; i<6; i++) {
+// CON WHILE
+var listaNumeri = [];
+var i = 0;
+while (i<6) {
   var numeroUtente = parseInt(prompt('inserisci un numero'));
   console.log(numeroUtente);
-  if (numeroUtente%2 != 0){
+  if (numeroUtente%2 != 0) {
     listaNumeri.push(numeroUtente);
     console.log(listaNumeri);
   }
+  i++;
 }
