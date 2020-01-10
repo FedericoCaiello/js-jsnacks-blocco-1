a7e005a3723e7af9631cddb508ce8575a60ee2ce
Federico <federico.caiello@gmail.com>
2020-01-10 17:09:20 +0100

    primo jsnack

---
+++ jsnack1/js/script.js
@@ -0,0 +1,14 @@
+// L’utente inserisce due numeri in successione, con due
+// prompt.
+var numeroUno = parseInt(prompt('inserisci un numero'));
+console.log(numeroUno);
+var numeroDue = parseInt(prompt('inserisci un altro numero'));
+console.log(numeroDue);
+// Il software stampa il maggiore.
+if (numeroUno == numeroDue) {
+  console.log(numeroUno);
+}else if (numeroUno < numeroDue) {
+  console.log(numeroDue);
+}else {
+  console.log(numeroUno);
+}
