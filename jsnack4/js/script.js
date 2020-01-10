66f646023e36c6f788c2781cc5265f5b359afdc9
Federico <federico.caiello@gmail.com>
2020-01-10 18:01:20 +0100

    jsnack4

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -3,3 +3,18 @@
 // può partecipare o no alla festa.
 var nomi = ['marco', 'nino', 'fede', 'lollo'];
 console.log(nomi);
+// chiedi all’utente il suo nome
+var nomeInvitato = prompt('il tuo nome');
+console.log(nomeInvitato);
+//comunicagli se può partecipare o no alla festa.
+var invitato = false;
+for (var i=0; i<nomi.length; i++) {
+  console.log(nomi[i]);
+  if (nomi[i] == nomeInvitato) {
+    invitato = true;
+    console.log('puoi entrare');
+  } else {
+    console.log('non puoi entrare non sei un invitato');
+  }
+}
+console.log(invitato);
