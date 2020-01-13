b9b55bd9f62ade811077796f0bae66dac7b47455
Federico <federico.caiello@gmail.com>
2020-01-13 11:56:07 +0100

    jsnack 9 aggiunto un alert

--- jsnack9/js/script.js
+++ jsnack9/js/script.js
@@ -10,4 +10,5 @@
 
 var frasi = ['Oggi troverai una cosa dimenticata', 'Ciò che è fatto è fatto', 'Il buon giorno si vede dal mattino', 'Sicuro di aver chiuso il gas?', 'Chi semina vento raccoglie tempesta', 'Il mattino ha l/oro in bocca', 'Tanto va la gatta al lardo che ci lascia lo zampino'];
 var who = Math.floor((Math.random() * 100000) % frasi.length);
+alert(frasi[who]);
 console.log(frasi[who]);
